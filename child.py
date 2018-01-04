b7c6799f6b1805c24c65429fac86072f398b7aab
tianfeihan <1961807207@qq.com>
2018-01-04 16:52:01 +0800

    database practice

--- child.py
+++ child.py
@@ -1,1 +1,17 @@
-import  b7e078e27f11ed49d55125fdb2fb1324
+# 训练第十三章部分--数据库
+import sqlite3
+conn=sqlite3.connect("test.db")
+print("Opened database successfully")
+c=conn.cursor()
+c.execute("DELETE FROM COMPANY  WHERE  ID=4")
+conn.commit()
+print(conn.total_changes)
+cursor = c.execute("SELECT id, name, address, salary  from COMPANY")
+for row in cursor:
+   print ("ID = ", row[0])
+   print ("NAME = ", row[1])
+   print ("ADDRESS = ", row[2])
+   print ("SALARY = ", row[3], "\n")
+
+# print "Operation done successfully";
+conn.close()
